c91c4498cbd8d7d049410b93f6e07f7523ebaeb2
Aran Haber <ah2204@cam.ac.uk>
2022-12-16 15:29:34 +0000

    write code to pass tests

--- image.py
+++ image.py
@@ -1,11 +1,30 @@
 from PIL import Image
 
 def read_image(filename):
-    """read image and get relevant data
-    return image as a PIL object and size (x,y)"""
+    """read image from jpg file: returns image as PIL object and size (x,y)"""
+    im = Image.open(filename) # str: filename
+    (x,y) = im.size
+    return(im, (x,y))
 
-def resize_image(im, size):
-    """resize image appropriately for reasonably sized excel file"""
+def resize_image(im, original_size):
+    """resize image appropriately for reasonably sized excel file\n
+    return list of pixel values and new size"""
+    (x,y) = original_size
+    for i in range(3): # brute force to resize this particular image to an appropriate size for now
+        x//=2
+        y//=2
+        im = im.resize((x,y))
+    return(list(im.getdata()), im.size)
 
-def pixel_lists(pixels, x, y):
+def pixel_lists(pixels, size):
     """split pixel values into lists of correct size and shape for spreadsheet"""
+
+    lists = [] # create empty lists
+    (x,rows) = size
+    for i in range(rows):
+        # for each row (y) in image, create three new rows
+        # one row for each colour r,g,b
+        for j in range(3):
+            colour = [k[j] for k in pixels[x*i:x*(i+1)]]
+            lists.append(colour)
+    return(lists)
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,7 +1,35 @@
 import pandas as pd
 
-def create_df():
+def create_df(lists):
     """create data frame with relevant formatting"""
+    df = pd.DataFrame(lists)
+    # add option for vert/horizontal pixel alignment using df.transpose()
+    return(df)
 
-def create_sheet(lists):
+def create_sheet(df):
     """create spreadsheet with given pixel values"""
+    
+    # with ... as ... should close the sheet automatically when done
+    with pd.ExcelWriter('imageresult.xlsx') as writer:
+        df.to_excel(writer, header=False, index=False) # create spreadsheet
+
+        worksheet = writer.sheets['Sheet1'] # set up worksheet
+
+        (max_row, max_col) = df.shape
+        # set up conditional formatting
+        for row in range(max_row):
+            # rows are zero indexed here
+            if row % 3 == 0:
+                colours = ['#000000','#800000','#FF0000'] # red
+            elif row % 3 == 1:
+                colours = ['#000000','#008000','#00FF00'] # green
+            elif row % 3 == 2:
+                colours = ['#000000','#000080','#0000FF'] # blue
+            # cells should behave as pixels, colour intensity depending on numerical value in each cell
+            worksheet.conditional_format(row,0,row,max_col, {'type': '3_color_scale',
+                        'min_type': 'num', 'min_value': 0,
+                        'mid_type': 'num', 'mid_value': 128,
+                        'max_type': 'num', 'max_value': 255,
+                                    'min_color': colours[0],
+                                    'mid_color':colours[1],
+                                    'max_color':colours[2]})
