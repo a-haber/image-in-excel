015ef9d5ea0dacd0ce6ee596fbb18d5be223025e
Aran Haber <ah2204@cam.ac.uk>
2022-12-16 15:28:48 +0000

    improve tests

--- test_modules.py
+++ test_modules.py
@@ -18,21 +18,24 @@ def test_read_image():
 
 def test_resize_image():
     im, size = read_image("tweety.jpg")
-    pix_val = resize_image(im, size)
+    pix_val, newsize = resize_image(im, size)
     assert isinstance(pix_val, list)
     assert isinstance(pix_val[0], tuple)
     assert (size[0]*size[1] >= len(pix_val)) 
+    assert (size[0] >= newsize[0])
 
 def test_pixel_lists():
     pixels = [(255,255,255)]*4020
     (x,y) = (67,60)
-    lists = pixel_lists(pixels, x, y)
+    lists = pixel_lists(pixels, (x, y))
     assert(len(lists) == 3*y)
     assert(len(lists[0]) == x)
     
 def test_create_df():
     (x,y) = (67,60)
-    lists = [255]* x
-    lists = [lists]*y
-    df = pd.DataFrame(lists)
-    assert(df.shape == (3*y,x))
+    lists = [255] * x
+    lists = [lists] * y
+    df = create_df(lists)
+    assert (df.shape == (y,x))
+
+test_create_df()
